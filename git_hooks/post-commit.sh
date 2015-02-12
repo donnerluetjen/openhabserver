4667e7c5b4b0562df6076d23c7342e043e6849bd
devaa <devaa@donnerluetjen.de>
2015-02-12 16:20:58 +0100

    fixed the java.lang.RuntimeException
    commented out demo.rules
    reinstated the wunderground.xsl
    reinstated the weather frame in haus sitemap

--- git_hooks/post-commit.sh
+++ git_hooks/post-commit.sh
@@ -3,3 +3,7 @@
 # this file is copied into ../.git/hooks and made executable
 
 echo "Post commit routine started"
+
+# there is no local post update hook
+# so I could create a script that runs the push and then
+# when push has completed, runs the cap deploy command
